691f8277b3fadcce7ceab2bf0123ddda74c47454
Samiul Fahad <samiul_fahad@yahoo.com>
2022-07-29 11:06:00 +0600

    Production

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
-
 app.use(express.static(publicDirectoryPath))
+
 
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
